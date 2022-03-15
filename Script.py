17e440c559887aeb1e64e531b6a926dec5ecba48
Kevin Verquerre <verquerre.kevin@gmail.com>
2022-03-15 11:39:07 +0100

    first step finished get all data from a product page

--- Script.py
+++ Script.py
@@ -1,2 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+
+def get_data_from_product_page(url: str) -> dict[str, str]:
+    """
+    Scrap the url page and extract all the product data and write them
+    in product_data dict.
+    :param url:
+    :return product_data:
+    """
+    # Extraction
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Transformation
+    product_data = {'product_page_url': url}
+    product = soup.find('div', class_='content')
+    product_data['image_url'] = urljoin(url, product.find('img')['src'])
+    if product.find('p', class_=''):
+        product_data['product_description'] = product.find('p', class_='').text
+    table_lines = product.find('table').find_all('tr')
+    product_data['universal_product_code'] = table_lines[0].find('td').text
+    product_data['price_excluding_tax'] = table_lines[2].find('td').text.replace('£', '')
+    product_data['price_including_tax'] = table_lines[3].find('td').text.replace('£', '')
+    product_data['review_rating'] = table_lines[6].find('td').text
+    product_data['title'] = product.find('h1').text
+    availability = product.find('p', class_='availability')
+    if 'instock' in availability['class']:
+        product_data['number_available'] = availability.text.replace(' ', '') \
+            .replace('\n\n\nInstock(', '').replace('available)\n\n', '')
+    else:
+        product_data['number_available'] = '0'
+    product_data['review_rating'] = product.find('p', class_='star-rating')['class'][1]
+    product_data['category'] = soup.find('ul', class_='breadcrumb').find_all('li')[2].text.replace('\n', '')
+    return product_data
+
+
+def main():
+    print(get_data_from_product_page("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"))
+
+
+if __name__ == '__main__':
+    main()
